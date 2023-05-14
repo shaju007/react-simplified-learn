2a4962f6b38426e67a5c7166ec4aba982590258a
Shaju <shaju007@users.noreply.github.com>
2023-05-14 13:43:16 +0200

    Chore: Course complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react';
 import './style.css'
+import { NewTodoForm } from './NewTodoForm';
+import { TodoList } from './TodoList';
 
-function App(){
-  return 'Hi'
+export default function App(){
+  const [todos, setTodos] = useState(()=>{
+    const localValue = localStorage.getItem('ITEMS')
+    if(localValue === null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(()=>{
+localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title){
+     setTodos(currentTodos =>{
+          return [
+            ...currentTodos, {id: crypto.randomUUID(), title, completed: false}
+          ]
+        })
+  }
+  
+
+  function toggleTodo(id, completed){
+   setTodos(currentTodos=>{
+    return currentTodos.map(todo =>{
+      if(todo.id === id){
+        return { ...todo, completed}
+      }
+      return todo
+    })
+   })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos=>{
+      return currentTodos.filter(todo=> todo.id!=id)
+    })
+
+  }
+  return (
+    <>
+    <NewTodoForm onSubmit={addTodo}/>
+    <h1>To do list</h1>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+  </>
+  )
 }
-
-export default App
